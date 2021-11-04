f625d9e6cd22bc6b6485286675308a263b41a3d4
seongjun_kim <lewisel@naver.com>
2021-11-04 16:54:53 +0900

    13. Finishing touches!
     - 컴포넌트 분리: Greeting 내용을 CardContent 컴포넌트로 분리
     - expanded 상태를 Greeting에서 관리하고, 콜백을 넘겨주도록 수정
     - icon 적용: 아이콘 표시하도록 수정/기존 문구는 strings.xml에 정의하고 contentDescription으로 활용

--- week 1-Jetpack Compose basics/MY_BasicsCodelab/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ week 1-Jetpack Compose basics/MY_BasicsCodelab/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,19 +5,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -56,44 +62,64 @@ private fun Greetings(names: List<String> = List(1000) { "$it" }) {
 
 @Composable
 private fun Greeting(name: String) {
-    val expanded = rememberSaveable { mutableStateOf(false) }
-    val extraPadding by animateDpAsState(
-        if(expanded.value) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
+
+    var expanded by rememberSaveable { mutableStateOf(false) }
     val color by animateColorAsState(
-        targetValue = if (expanded.value) MaterialTheme.colors.secondary else MaterialTheme.colors.primary,
+        targetValue = if (expanded) MaterialTheme.colors.secondary else MaterialTheme.colors.primary,
         animationSpec = tween(durationMillis = 250)
     )
 
-    Surface(
-        color = color,
+    Card(
+        backgroundColor = color,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier
+        CardContent(name, expanded, onIconClicked = { expanded = !expanded })
+    }
+}
+
+@Composable
+private fun CardContent(name: String, expanded: Boolean, onIconClicked: () -> Unit) {
+    Row(modifier = Modifier
+        .padding(12.dp)
+        .animateContentSize(
+            animationSpec = spring(
+                dampingRatio = Spring.DampingRatioMediumBouncy,
+                stiffness = Spring.StiffnessLow
+            )
+        )
+    ) {
+        Column(
+            modifier = Modifier
                 .weight(1f)
-                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-            ) {
-                Text(text = "Hello,")
-                Text(text = "$name", style = MaterialTheme.typography.h4.copy(
-                    fontWeight = FontWeight.ExtraBold
-                ))
+                .padding(12.dp)
+
+        ) {
+            Text(text = "Hello,")
+            Text(text = "$name", style = MaterialTheme.typography.h4.copy(
+                fontWeight = FontWeight.ExtraBold
+            ))
+            if (expanded) {
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4),
+                )
             }
-            OutlinedButton(
-                onClick = { expanded.value = !expanded.value },
-                modifier = Modifier.padding(top=30.dp)) {
-                Text(if(expanded.value) "Show less" else "Show more")
-            }
+        }
+        IconButton(onClick = onIconClicked) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
+            )
         }
     }
 }
 
 @Composable
-fun OnboardingScreen(onContinueClicked: () -> Unit) {
+private fun OnboardingScreen(onContinueClicked: () -> Unit) {
     Surface {
         Column(
             modifier =Modifier.fillMaxSize(),
