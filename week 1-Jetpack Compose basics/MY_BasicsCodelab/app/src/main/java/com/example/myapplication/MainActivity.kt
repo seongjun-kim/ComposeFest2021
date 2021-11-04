224b5ffef60ce4b60bc8bac7dcb4d9482b704d72
seongjun_kim <lewisel@naver.com>
2021-11-04 14:41:42 +0900

    6. Creating columns and rows
     - OutlinedButton 상단 여백 추가

--- week 1-Jetpack Compose basics/MY_BasicsCodelab/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ week 1-Jetpack Compose basics/MY_BasicsCodelab/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -25,23 +29,38 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-private fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Greeting("Android")
+private fun MyApp(names: List<String> = listOf("FIRST", "SECOND")) {
+    Column {
+        for (name in names) {
+            Greeting(name)
+        }
     }
 }
 
 @Composable
 private fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary) {
-        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello,")
+                Text(text = "$name!")
+            }
+            OutlinedButton(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.padding(top=4.dp)) {
+                Text("Show more")
+            }
+        }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 private fun DefaultPreview() {
     MyApplicationTheme {
-        Greeting("Android")
+        MyApp(listOf("World", "Compose"))
     }
 }
