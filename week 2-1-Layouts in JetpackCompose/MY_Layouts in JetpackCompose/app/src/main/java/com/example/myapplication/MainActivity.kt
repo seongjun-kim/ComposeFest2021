89a541b6af288353d18bbbe003327e1b3269a436
seongjun_kim <lewisel@naver.com>
2021-12-06 09:49:16 +0900

    2-5.Material Components

--- week 2-1-Layouts in JetpackCompose/MY_Layouts in JetpackCompose/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/MY_Layouts in JetpackCompose/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
@@ -27,29 +29,52 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    //Greeting("Android")
-                    PhotographerCard()
-                }
+                LayoutsCodelab()
             }
         }
     }
 }
 
 @Composable
+fun LayoutsCodelab() {
+    Scaffold(topBar = {
+        TopAppBar(
+            title = {Text(text = "LayoutsCodelab")},
+            actions = {
+                IconButton(onClick = {}){
+                    Icon(Icons.Filled.Favorite, contentDescription = null)
+                }
+            }
+        )
+    }) { innerPadding ->
+        BodyContent(Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier){
+    Column(modifier = modifier) {
+        Text(text = "Hi there!")
+        Text(text = "Thanks for going through the Layouts codelab")
+    }
+}
+
+@Composable
 fun PhotographerCard(modifier : Modifier = Modifier) {
-    Row(modifier
-        .padding(8.dp)
-        .clip(RoundedCornerShape(4.dp))
-        .background(MaterialTheme.colors.surface)
-        .clickable(onClick = {})
-        .padding(16.dp)
+    Row(
+        modifier
+            .padding(8.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
+            .clickable(onClick = {})
+            .padding(16.dp)
     ){
         Surface(modifier = Modifier.size(50.dp), shape = CircleShape, color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)) {
             // image
         }
-        Column(modifier = Modifier.padding(start = 8.dp).align(Alignment.CenterVertically)){
+        Column(modifier = Modifier
+            .padding(start = 8.dp)
+            .align(Alignment.CenterVertically)){
             Text("Alfred Sisley", fontWeight = FontWeight.Bold)
             // LocalContentAlpha
             CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
@@ -60,10 +85,18 @@ fun PhotographerCard(modifier : Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
+//@Composable
+//fun PhotographerCardPreview() {
+//    MyApplicationTheme {
+//        PhotographerCard()
+//    }
+//}
+
+@Preview
 @Composable
-fun PhotographerCardPreview() {
+fun LayoutsCodelabPreview() {
     MyApplicationTheme {
-        PhotographerCard()
+        LayoutsCodelab()
     }
 }
